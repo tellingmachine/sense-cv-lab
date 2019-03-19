36b81777388c5c360250ab3d852d557e2d998bf7
tellingmachine <klaus@tellingmachine.com>
2019-03-19 15:34:24 -0700

    infinite while 2

--- brokenled.py
+++ brokenled.py
@@ -4,15 +4,38 @@ import time
 sense = SenseHat()
 sense.low_light = True
 
-r = 255
-g = 255
-b = 255
+r = [255,0,0]
+o = [255,127,0]
+y = [255,255,0]
+g = [0,255,0]
+b = [0,0,255]
+i = [75,0,130]
+v = [159,0,255]
+e = [0,0,0]
 
-sense.clear((r, 0, b))
+color = r
+ 
+image = [
+e,e,e,e,e,e,e,e,
+e,e,e,r,r,e,e,e,
+e,r,r,o,o,r,r,e,
+r,o,o,y,y,o,o,r,
+o,y,y,g,g,y,y,o,
+y,g,g,b,b,g,g,y,
+b,b,b,i,i,b,b,b,
+b,i,i,v,v,i,i,b
+]
+
+x = 0
+y = 0
+good_over_bad = 10
+delay = 3
+
+sense.clear(color)
 
 while True:
 
-    sense.set_pixel(2, 2, [0, 0, 0])
-    time.sleep(3)
-    sense.set_pixel(2, 2, [r, 0, b])
-    time.sleep(3)
+    sense.set_pixel(x, y, e)
+    time.sleep(delay)
+    sense.set_pixel(x, y, color)
+    time.sleep(delay)
